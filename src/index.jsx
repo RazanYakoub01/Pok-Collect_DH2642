1da1945df3e3063faeb879a7f8c5c9af93e6dbbe
18alba1 <alexanderbarhado@gmail.com>
2023-11-27 15:34:25 +0100

    teacherFetch

--- src/index.jsx
+++ src/index.jsx
@@ -1,3 +1,4 @@
+import "/src/teacherFetch.js"; // protection against fetch() in infinite re-render
 import {createRoot} from "react-dom/client";
 createRoot(document.getElementById('root'))
     .render(<div>hello world!</div>);  
