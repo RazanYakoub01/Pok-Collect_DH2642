450c3b58c28a7b21ba6d0124629ce8ac63ca83b0
Seemamian <bashir.seema@gmail.com>
2023-12-13 19:43:56 +0100

    updated AboutUs Presenters
    AboutUsPresenters

--- src/presenters/AboutUsPresenter.jsx
+++ src/presenters/AboutUsPresenter.jsx
@@ -1,11 +1,49 @@
 import AboutUsView from '../views/aboutUsView.jsx';
 import { observer } from "mobx-react-lite";
 
+export default observer(function AboutUsPresenter(props) {
+  const memberArray = [
+    {
+      img: "Seema.png",
+      name: "Seema", 
+      surname: "Bashir", 
+      email: "sfbashir@kth.se", 
+      linkedIn: "https://www.linkedin.com/in/seema-bashir-a2b10b214/",
+      program: "Computer Science",
+      LO1: "I think this project has been a great way to improve my knowledge in React",
+      LO2: "Furthermore, I've learnt a lot about how to create a nice user experience!",
+    },
+    {
+      img: "Alex.png",
+      name: "Alex", 
+      surname: "Barhado", 
+      email: "XX", 
+      linkedIn: "X",
+      program: "Computer Science ",
+      LO1: "X",
+      LO2: "X",
+    },
+    {
+      img: "Razan.png",
+      name: "Razan", 
+      surname: "Yakoub", 
+      email: "XX", 
+      linkedIn: "X",
+      program: "Computer Science ",
+      LO1: "X",
+      LO2: "X",
+    },
+    {
+      img: "Elias.png",
+      name: "Amani", 
+      surname: "Barhado", 
+      email: "XX", 
+      linkedIn: "X",
+      program: "Computer Science ",
+      LO1: "X",
+      LO2: "X",
+    },
+  ];
 
-export default
-observer(            
-function AboutUsPresenter(props){ 
-  return <AboutUsView />;
-
-}
-);
+  return <AboutUsView memberArray={memberArray} />;
+});
