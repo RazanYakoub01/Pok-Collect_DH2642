2b5d19fc4a472ca0c59821db41dcc767673a940e
Seemamian <bashir.seema@gmail.com>
2023-12-15 11:34:03 +0100

    aboutUS view (not completed)
    hi

--- src/views/aboutUsView.jsx
+++ src/views/aboutUsView.jsx
@@ -1,41 +1,47 @@
 import React from 'react';
 import "/src/aboutUs.css";
 import "/src/textFonts.css";
-import temp1 from '/src/aboutUsImages/temp1.png';
-import temp2 from '/src/aboutUsImages/temp2.jpg';
-import ti from '/src/aboutUsImages/ti.jpg';
 
-
-const AboutUsView = () => {
+const AboutUsView = ({ memberArray }) => {
   return (
-    <div>
-      <div className="titleContainer">
-      <img src={ti} alt="Title Image" />
-      <h1 className="titleFont">About Us</h1>
+    <div className="flex-row-homepage">
+      <div className="flex-column-aboutus">
+        <h className="App-name">About us</h>
+        <div className="description-font-20px">
+        Welcome to PokeCollect, where our passion for Pokémon comes to life in the digital realm! Our journey began with a simple yet profound goal: to create an interactive online space that celebrates the enchanting world of Pokémon.
+        </div>
+
+        <a style={{ fontSize: "2vw" }} href="https://github.com/RazanYakoub01/PokeCollect_DH2642">GitHub</a>
+
+        <h1 className="meet-the-team">MEET THE TEAM</h1>
+        <div style={{ display: "flex", flexDirection: "column", gap: "2vw" }}>
+          {memberArray.map((member, index) => {
+            return (
+              <div key={index} className="flex-row-profile square-profile">
+                <span className="flex-column-profile">
+                  <img className="profile-image" src={member.img} height={200} alt={`${member.name} ${member.surname}`} />
+                  <h1 className="profile-name"> {member.name}</h1>
+                  <h1 className="profile-name"> {member.surname}</h1>
+                  <h1 className="profile-email">{member.email}</h1>
+                  <a className="profile-email" href={member.linkedIn}>My LinkedIn</a>
+                </span>
+                <span className="more-about-member">
+                  <p className="profile-bold">Programe: <p className="description-font-20px-profile">{member.program}</p></p><br /> <br />
+                  <p className="profile-bold">Learning Objectives: </p>
+                  <ul className="LO-list">
+                    <li>{member.LO1}</li>
+                    <li>{member.LO2}</li>
+                  </ul>
+                </span>
+              </div>
+            )
+          })}
+        </div>
       </div>
-      <p>If you are intrested and want to learn more about our project, please visit our git-repository:</p>
-      <a href="https://github.com/RazanYakoub01/PokeCollect_DH2642">
-        GitHub
-      </a>
-      <h2>Contributors:</h2>
-      <div className="contributor">
-      <img src={temp1}/>
-      <div>email, program, what I want to learn, favourite pokemon</div>
-      </div>
-      <div className="contributor">
-      <img src={temp1}/>
-      <div>email, program, what I want to learn, favourite pokemon</div>
-      </div>
-      <div className="contributor">
-      <img src={temp2}/>
-      <div>email, program, what I want to learn, favourite pokemon</div>
-      </div>
-      <div className="contributor">
-      <img src={temp2}/>
-      <div>email, program, what I want to learn, favourite pokemon</div>
-      </div>
+      <div className="grarphic-background"></div>
     </div>
   );
 };
 
 export default AboutUsView;
+
