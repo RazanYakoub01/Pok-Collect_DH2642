8fdf4efca47c62df4e1e3eb3649c18facfddbc4a
Seemamian <bashir.seema@gmail.com>
2023-12-15 21:32:18 +0100

    final HomePage

--- src/presenters/HomePresenter.jsx
+++ src/presenters/HomePresenter.jsx
@@ -1,14 +1,10 @@
 // src/presenters/HomePresenter.jsx
 import React from 'react';
-import { observer } from "mobx-react-lite";
-import HomeView from '../views/homeView.jsx';
+import { observer } from 'mobx-react-lite';
+import HomeView from '../views/homeView';
 
+const HomePresenter = observer((props) => {
+  return <HomeView /* data={homeData} */ />;
+});
 
-export default
-observer(             // needed for the presenter to update (its view) when relevant parts of the model change
-function HomePresenter(props){ 
-
-  return <HomeView /* data={homeData}*/  />;
-
-}
-);
+export default HomePresenter;
