a722f120dbc096b6c33110e67f47031a7d1e8ccb
Seemamian <bashir.seema@gmail.com>
2023-12-15 21:27:16 +0100

    final Store Presenter

--- src/presenters/StorePresenter.jsx
+++ src/presenters/StorePresenter.jsx
@@ -4,20 +4,22 @@ import { observer } from 'mobx-react-lite';
 import { packs } from '../storeData';
 import { useAuthentication } from '../services/authService';
 
-
-export default observer(function StorePresenter(props) {
-  
+const StorePresenter = observer((props) => {
   const { user } = useAuthentication();
 
   const addToCartACB = (selectedPack) => {
-    props.model.addItem(selectedPack,user);
+    props.model.addItem(selectedPack, user);
   };
 
   const totalItemsInCart = props.model.getTotalItemsInCart();
 
-  return <StoreView 
-  packs={packs} 
-  addToCartACB={addToCartACB}
-  totalItemsInCart={totalItemsInCart}
-  />;
+  return (
+    <StoreView
+      packs={packs}
+      addToCartACB={addToCartACB}
+      totalItemsInCart={totalItemsInCart}
+    />
+  );
 });
+
+export default StorePresenter;
