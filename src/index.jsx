01e3e9ba43548b31bdb80283b0e462f723655f24
RazanYakoub01 <131905291+RazanYakoub01@users.noreply.github.com>
2023-12-17 21:42:48 +0100

    fixed bug , dont log out upon refresh anymore

--- src/index.jsx
+++ src/index.jsx
@@ -1,29 +1,23 @@
 import { observable, configure, reaction } from "mobx";
 import model from './models/pokeModel.js';
-//import connectToFirebase from './firebaseModel.js';
 import db from '/src/firebaseModel';
+import  handleAuthStateChange from '/src/services/authService.js';
 
 
 configure({ enforceActions: "never", });  // we don't use Mobx actions
 const reactiveModel= observable(model);
 
 
-import {createElement} from "react";
-window.React= {createElement:createElement}; // needed in the lab because it works with both React and Vue
-
 import {createRoot} from "react-dom/client";
 import ReactRoot from "/src/ReactRoot.jsx";
 
 createRoot(document.getElementById('root')).render(<ReactRoot model={reactiveModel}/>);  // mounts the app in the page DIV with the id "root"
-// to see the DIV, look at react.html in the developer tools Sources
-// react.html, with the content <div id="root"></div> is configured in vite.config.js
 
+handleAuthStateChange();
 
 reactiveModel.getPokemonData();
 
-
 // ------ for debug purposes ----------
-//window.myModel= model;             // make the model available in the Console
 window.myModel= reactiveModel;
 
 db.connectToFirebase(reactiveModel,reaction);
