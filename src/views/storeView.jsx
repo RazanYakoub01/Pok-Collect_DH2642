e15ba3c89edec160ff2e5a63e2c2ce1f7599cbcf
Seemamian <bashir.seema@gmail.com>
2023-12-19 13:35:03 +0100

    Store view bug fixed

--- src/views/storeView.jsx
+++ src/views/storeView.jsx
@@ -1,3 +1,4 @@
+import React, { useState } from 'react';
 import Popup from 'reactjs-popup';
 import { Link, useNavigate } from 'react-router-dom';
 import 'reactjs-popup/dist/index.css';
@@ -7,67 +8,114 @@ import coin from '/src/storeImages/coin.png';
 import cart2 from '/src/storeImages/cart2.png';
 import s from '/src/storeImages/s.png';
 
-// packsView.jsx
-import React from 'react';
 
-function PacksView({
-  packs,
-  onOpenPack,
-  handleAddToCart,
-  handleNavigateToCart,
-  getGenerationClass,
-  selectedPack,
-  setSelectedPack,
-}) {
+function StoreView(props) {
+  const navigator = useNavigate();
+  const [selectedPack, setSelectedPack] = useState(null);
+
+  const handleAddToCart = (pack) => {
+    props.addToCartACB(pack);
+    setSelectedPack(pack);
+  };
+
+  const handleNavigateToCart = () => {
+    navigator('/cart');
+    setSelectedPack(null);
+  };
+
+
+  const getGenerationClass = (pack) => {
+    switch (pack.id) {
+      case 1:
+        return 'gen1';
+      case 2:
+        return 'gen2';
+      case 3:
+        return 'gen3';
+      case 4:
+        return 'gen4';
+      case 5:
+        return 'gen5';
+      case 6:
+        return 'gen6';
+      case 7:
+        return 'gen7';
+      case 8:
+        return 'gen8';
+
+      case 9:
+        return 'gen9'
+    
+      case 10:
+        return 'legendary';
+
+      default:
+        return ''; 
+    }
+  };
+  
+
   return (
-    <div>
-      {/* Render your packs here */}
+    <div className="shop">
+      <div className="shopTitleContainer">
+        <img src={s} className="titleImage" alt="Shop Title" />
+        <h1 className="titleFont">PokéCollect Shop</h1>
+      </div>
+      <div className="balance">
+        <p>
+          Current Balance: {props.balance} coins <img src={coin} alt="Coin Icon" />
+        </p>
+        <p>
+          Cart - Check out:
+          <Link to="/cart" className="cart-link">
+            <img src={cart2} style={{ width: '30px', height: '30px' }} alt="Cart Icon" />
+            ({props.totalItemsInCart})
+          </Link>
+        </p>
+      </div>
       <div className="packs">
-        {packs.map((pack) => (
+        {props.packs.map((pack) => (
           <div key={pack.id} className={`pack ${getGenerationClass(pack)}`}>
-            {/* Render pack details */}
-            <h2>{pack.packName}</h2>
-            <div>
-              Price: {pack.price} coins <img src="/src/storeImages/coin.png" alt="Coin Icon" />
-            </div>
-
-            {/* Add to Cart button */}
-            <button className="storeButton" onClick={() => handleAddToCart(pack)}>
-              Add to Cart
-            </button>
-
-            {/* Popup */}
-            {selectedPack && (
-              <Popup
-                open={selectedPack !== null}
-                closeOnDocumentClick
-                onClose={() => setSelectedPack(null)}
-              >
-                <div className="modal">
-                  <button className="close" onClick={() => setSelectedPack(null)}>
-                    &times;
-                  </button>
-                  <div className="header">
-                    <div className="content">
-                      <div className="added-to-cart">
-                        {selectedPack && `${selectedPack.packName} added to cart`}
+            <img src={pack.packImage} alt={pack.packName} />
+            <div className="packDetails">
+              <h2>{pack.packName}</h2>
+              <div>
+                Price: {pack.price} coins <img src={coin} alt="Coin Icon" />
+              </div>
+              <button className="storeButton" onClick={() => handleAddToCart(pack)}>
+                Add to Cart
+              </button>
+              {selectedPack && (
+                <Popup open={selectedPack !== null} closeOnDocumentClick onClose={() => setSelectedPack(null)}>
+                  <div className="modal">
+                    <button className="close" onClick={() => setSelectedPack(null)}>
+                      &times;
+                    </button>
+                    <div className="header">
+                      <div className="content">
+                        <div className="added-to-cart">
+                          {selectedPack && `${selectedPack.packName} added to cart`}
+                        </div>
                       </div>
                     </div>
+                    <div className="actions">
+                      <button
+                        className="storeButton"
+                        onClick={() => setSelectedPack(null)}
+                      >
+                        CONTINUE SHOPPING
+                      </button>
+                      <button
+                        className={`storeButton cart-button ${getGenerationClass(selectedPack)}`}
+                        onClick={handleNavigateToCart}
+                      >
+                        CART
+                      </button>
+                    </div>
                   </div>
-                  <div className="actions">
-                    <button className="storeButton" onClick={() => setSelectedPack(null)}>
-                      CONTINUE SHOPPING
-                    </button>
-                    <button
-                      className={`storeButton cart-button ${getGenerationClass(selectedPack)}`}
-                      onClick={handleNavigateToCart}
-                    >
-                      CART
-                    </button>
-                  </div>
-                </div>
-              </Popup>
-            )}
+                </Popup>
+              )}
+            </div>
           </div>
         ))}
       </div>
@@ -75,4 +123,4 @@ function PacksView({
   );
 }
 
-export default PacksView;
+export default StoreView;
