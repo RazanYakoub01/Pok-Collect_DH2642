fd85f105560fc69cb68ceebfaaf5d681bc859177
RazanYakoub01 <131905291+RazanYakoub01@users.noreply.github.com>
2023-12-19 14:55:36 +0100

    fixed linkedin and github icons in aboutus

--- src/views/aboutUsView.jsx
+++ src/views/aboutUsView.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import "/src/aboutUs.css";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faGithub, faLinkedin } from '@fortawesome/free-brands-svg-icons';
 import tiImage from "/src/aboutUsImages/ti.jpg";
 
 const AboutUsView = ({ memberArray }) => {
@@ -14,7 +16,7 @@ const AboutUsView = ({ memberArray }) => {
           Welcome to PokeCollect, where our passion for Pokémon comes to life in the digital realm! Our journey began with a simple yet profound goal: to create an interactive online space that celebrates the enchanting world of Pokémon.
         </div>
 
-        <a style={{ fontSize: "2vw" }} href="https://github.com/RazanYakoub01/PokeCollect_DH2642">GitHub</a>
+        <a style={{ fontSize: "2vw" }} href="https://github.com/RazanYakoub01/PokeCollect_DH2642"> <FontAwesomeIcon icon={faGithub} /> GitHub</a>
 
         <h1 className="meet-the-team">MEET THE TEAM</h1>
         <div style={{ display: "flex", flexDirection: "column", gap: "2vw" }}>
@@ -26,7 +28,9 @@ const AboutUsView = ({ memberArray }) => {
                   <h1 className="profile-name"> {member.name}</h1>
                   <h1 className="profile-name"> {member.surname}</h1>
                   <h1 className="profile-email">{member.email}</h1>
-                  <a className="profile-email" href={member.linkedIn}>My LinkedIn</a>
+                  <a className ="profile-email" href={member.linkedIn}>
+                    <FontAwesomeIcon icon={faLinkedin} className="linkedin-icon"  />
+                  </a>
                 </span>
                 <span className="more-about-member">
               <div style={{ display: "flex", flexDirection: "row" }}>
