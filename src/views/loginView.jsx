7d62cb8e1ecdff6edebf4b7ab161b7401bf09c07
Eliasamani <eliasamanii@gmail.com>
2023-12-21 01:45:56 +0100

    Login responsive

--- src/views/loginView.jsx
+++ src/views/loginView.jsx
@@ -1,10 +1,9 @@
-import React from 'react';
-import '/src/css/login.css';
-import hm4 from '/src/homepageImages/hm4.png';
+import React from "react";
+import "/src/css/login.css";
+import hm4 from "/src/homepageImages/hm4.png";
 import "/src/css/textFonts.css";
 
-export default function LoginView (props)  {
-
+export default function LoginView(props) {
   const fireGoogleSignIn = (event) => {
     props.onGoogleSignIn(event);
   };
@@ -12,15 +11,22 @@ export default function LoginView (props)  {
   return (
     <div>
       <h1>Login to your account </h1>
-      <button className='googleButton' onClick={fireGoogleSignIn}>
-        Sign in with Google
-      </button>
       <div>
-        <p>Please login in order to use our store and see your personal collection</p>
+      <div>
+        <p>
+          Please login in order to use our store and see your personal
+          collection
+        </p>
+      </div>
+        <div className="button-container">
+          <button className="googleButton" onClick={fireGoogleSignIn}>
+            Sign in with Google
+          </button>
+        </div>
       </div>
       <div>
-        <img className='homeImg' src={hm4}></img>
+        <img className="homeImg" src={hm4}></img>
       </div>
     </div>
   );
-};
+}
