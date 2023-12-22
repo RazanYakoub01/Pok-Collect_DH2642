96eb56fc45eeae15ddc147ef216b086db915ca8a
Seemamian <bashir.seema@gmail.com>
2023-12-22 18:37:54 +0100

    Resolve promis clean up

--- src/resolvePromise.js
+++ src/resolvePromise.js
@@ -1,27 +1,38 @@
+/**
+ * Sets up a promise and its handling in a given promise state.
+ * @param {Promise} promise - The promise to be resolved.
+ * @param {Object} promiseState - The state object to track the promise's status.
+ */
 export default function resolvePromise(promise, promiseState) {
-    //promiseState keeps track of the current active promise
-    promiseState.promise = promise;
-    //Ensure state is clean before handling new promise
-    promiseState.data = null;
-    promiseState.error = null;
-  
-    if (promiseState.promise) {
+  promiseState.promise = promise;
+  promiseState.data = null;
+  promiseState.error = null;
+
+  if (promiseState.promise) {
       promise.then(handlePromiseACB).catch(handleErrorACB);
-    }
-  
-    function handlePromiseACB(result) {
-      //Is the promise we are handling the same as the active one in promiseState?
-      if (promiseState.promise === promise) {
-        promiseState.data = result;
-        promiseState.error = null;
-      }
-    }
-  
-    function handleErrorACB(error) {
-      //Is the promise we are handling the same as the active one in promiseState?
-      if (promiseState.promise === promise) {
-        promiseState.error = error;
-        promiseState.data = null;
-      }
-    }
   }
+
+  /**
+   * Handles the successful resolution of the promise.
+   * @param {*} result - The result of the resolved promise.
+   */
+  function handlePromiseACB(result) {
+      
+      if (promiseState.promise === promise) {
+          promiseState.data = result;
+          promiseState.error = null;
+      }
+  }
+
+  /**
+   * Handles errors that occur during the promise resolution.
+   * @param {Error} error 
+   */
+  function handleErrorACB(error) {
+
+      if (promiseState.promise === promise) {
+          promiseState.error = error;
+          promiseState.data = null;
+      }
+  }
+}
