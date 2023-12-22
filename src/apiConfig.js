4a371f79d3cf96308c98faee8c606a75bc27cbf4
Seemamian <bashir.seema@gmail.com>
2023-12-22 18:25:15 +0100

    Comments

--- src/apiConfig.js
+++ src/apiConfig.js
@@ -1,1 +1,6 @@
-export const BASE_URL= "https://pokeapi.co/api/v2/";
+/*
+  This constant variable BASE_URL is defined to store the base URL of the PokeAPI.
+  The constant ensures that the value remains unchanged throughout the program.
+*/
+export const BASE_URL = "https://pokeapi.co/api/v2/";
+
--- src/firebaseModel.js
+++ src/firebaseModel.js
@@ -1,14 +1,14 @@
-// firebaseModel.js
-import { ref, get,set } from 'firebase/database';
+import { ref, get, set } from 'firebase/database';
 import { database } from './firebaseConfig.js';
 import model from './models/pokeModel.js';
 
+/* Function to read user data from Firebase based on the provided userId. */
 const readUserDataFromFirebase = async (userId) => {
   try {
     const userCartRef = ref(database, `users/${userId}/userData`);
     const snapshot = await get(userCartRef);
     if (snapshot.exists()) {
-      return persistenceToModel(snapshot.val(), model); 
+      return persistenceToModel(snapshot.val(), model);
     } else {
       console.log('No data available for this user.');
       return null;
@@ -19,19 +19,21 @@ const readUserDataFromFirebase = async (userId) => {
   }
 };
 
+/* Function to convert the application model to a persistence object for Firebase. */
 function modelToPersistence(model) {
   const persistenceObject = {
     cartItems: model.cartItems,
     balance: model.balance,
-    packs : model.packs,
+    packs: model.packs,
     currentPokemon: model.currentPokemon,
     collection: model.collection,
     lastLoginTime: model.lastLoginTime
-  }
+  };
 
   return persistenceObject;
 }
 
+/* Function to convert Firebase data to the application model. */
 function persistenceToModel(data, model) {
   data = data || {};
 
@@ -39,14 +41,17 @@ function persistenceToModel(data, model) {
   model.balance = data.balance || 200;
   model.packs = data.packs || [];
   model.collection = data.collection || [];
-  if (data.currentPokemon !== undefined){
+
+  // Set the current Pokemon in the model if available in the Firebase data.
+  if (data.currentPokemon !== undefined) {
     model.setCurrentPokemon(data.currentPokemon);
   }
+
   model.lastLoginTime = data.lastLoginTime || null;
   model.updateLastLoginAndBalance();
 }
 
-
+/* Function to write cart data to Firebase for the specified user. */
 const writeCartDataToFirebase = async (userId, model) => {
   try {
     const userCartRef = ref(database, `users/${userId}/userData`);
@@ -57,22 +62,38 @@ const writeCartDataToFirebase = async (userId, model) => {
   }
 };
 
-
+/* Function to connect the application model to Firebase using a watch function. */
 function connectToFirebase(model, watchFunction) {
+  /* Function to check the application model's data (ACB: Array of Cart and Balance). */
   function checkACB() {
     const cartItemQuantities = model.cartItems.map(item => item.quantity);
-    const data = [model.cartItems, model.balance,model.packs, model.currentPokemon, model.collection, model.lastLoginTime,cartItemQuantities];
+    const data = [
+      model.cartItems,
+      model.balance,
+      model.packs,
+      model.currentPokemon,
+      model.collection,
+      model.lastLoginTime,
+      cartItemQuantities
+    ];
     return data;
   }
+
+  /* Function to perform an effect based on changes in the ACB data. */
   function effectACB() {
     if (model.user && model.user.uid) {
       writeCartDataToFirebase(model.user.uid, model);
     }
   }
+  
+  watchFunction(checkACB, effectACB);
+
   if (model.user && model.user.uid) {
     readUserDataFromFirebase(model.user.uid);
   }
-  watchFunction(checkACB, effectACB);
 }
-
-export default { writeCartDataToFirebase , readUserDataFromFirebase, connectToFirebase };
+export default {
+  writeCartDataToFirebase,
+  readUserDataFromFirebase,
+  connectToFirebase
+};
